ee4d844b5b2822274a61336e95e1e854fa304ded
TommieTYX <cute_bear92@hotmail.com>
2018-06-13 17:14:09 +0800

    Modification to character Move mechanics
    - Walk now use velocity rather then transform to prevent clipping
    
    - Code refractoring

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,11 +9,16 @@ public class PlayerController : NetworkBehaviour {
     public float rotationSpeed = 55f;
     public LayerMask groundLayers;
 
+    public float distToWall = 1.0f;
+    public float distToSideWall = 0.4f;
+    public float snapToWallDist = 0.8f;
+
     private Vector3 forward, right, climbRight, up;
     private Rigidbody rb;
     private CapsuleCollider cc;
     private bool isClimbing = false;
-    private bool c2 = false;
+
+    [SerializeField] private bool debugMode = false;
 
     void Start () {
         forward = GameObject.Find("PlayerCamera").transform.forward;// new Vector3(0.6f, -0.5f, 0.6f);//Camera.main.transform.forward;
@@ -34,15 +39,21 @@ public class PlayerController : NetworkBehaviour {
             if (!isClimbing) {
                 Move();
             } else {
-                Climb();
+                //Climb();
             }
         }
     }
 
     void FixedUpdate() {
-        initClimb();
+        //if (!IsGrounded()) {
+            initClimb();
+        //}
         if (Input.anyKey && isLocalPlayer == true) {
             Jump();
+
+            if (isClimbing) {
+                Climb();
+            }
         }
     }
 
@@ -86,12 +97,17 @@ public class PlayerController : NetworkBehaviour {
 
         if (heading != Vector3.zero) { //Prevent Vector3 = 0 error when jumping
             transform.forward += heading * Time.deltaTime * rotationSpeed;
-            transform.position += rightMovement;
-            transform.position += upMovement;
-        }        
+            //Old method
+            //transform.position += rightMovement + upMovement;
+            //transform.position += upMovement;
+
+            //TODO: Using velocity can prevent clipping into object (NEW)
+            rb.velocity = new Vector3(heading.x * moveSpeed, rb.velocity.y, heading.z * moveSpeed);
+        }
     }
 
     void initClimb() {
+        
         Ray ray = new Ray(transform.position, transform.forward);
         Ray rayL = new Ray(transform.position - transform.TransformVector(Vector3.left * -0.5f), transform.forward);
         Ray rayR = new Ray(transform.position - transform.TransformVector(Vector3.right * -0.5f), transform.forward);
@@ -99,14 +115,14 @@ public class PlayerController : NetworkBehaviour {
         Ray rayRR = new Ray(transform.position - transform.TransformVector(Vector3.right * -0.2f), transform.right);
 
         RaycastHit hit;
+        
+        Debug.DrawRay(transform.position, transform.forward * distToWall, Color.green);
 
-        Debug.DrawRay(transform.position, transform.forward, Color.green);
+        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.left * -0.5f), transform.forward * snapToWallDist, Color.blue);
+        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.right * -0.5f), transform.forward * snapToWallDist, Color.red);
 
-        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.left * -0.5f), transform.forward, Color.blue);
-        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.right * -0.5f), transform.forward, Color.red);
-
-        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.left * -0.2f), -transform.right * 0.4f, Color.blue);
-        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.right * -0.2f), transform.right * 0.4f, Color.red);
+        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.left * -0.2f), -transform.right * distToSideWall, Color.blue);
+        Debug.DrawRay(transform.position - transform.TransformVector(Vector3.right * -0.2f), transform.right * distToSideWall, Color.red);
 
         Debug.DrawRay(transform.position - transform.TransformVector(Vector3.forward *-0.5f), transform.up, Color.black);
 
@@ -115,42 +131,41 @@ public class PlayerController : NetworkBehaviour {
         } else if (!isClimbing) {
             rb.drag = 0;
         }
-        
-        if (Physics.Raycast(ray, out hit, 0.6f)) {
+
+        if (Physics.Raycast(ray, out hit, distToWall)) {
             if (hit.transform.gameObject.GetComponent<EnvironmentObject>().isClimbable && isClimbing == false) {
                 isClimbing = true;
-                if (Physics.Raycast(rayL, out hit, 0.4f)) {
+                if (Physics.Raycast(rayL, out hit, snapToWallDist)) {
                     //Snap left when wall on nearer to left
                     transform.eulerAngles -= new Vector3(0, 45, 0);
-                } else if (Physics.Raycast(rayR, out hit, 0.4f)) {
+                } else if (Physics.Raycast(rayR, out hit, snapToWallDist)) {
                     //Snap right when wall nearer to right
                     transform.eulerAngles += new Vector3(0, 45, 0);
                 }
             }
 
-            Debug.Log("LL: " + Physics.Raycast(rayLL, out hit, 0.05f) + Input.GetKey(KeyCode.A) +  "____RR: " + Physics.Raycast(rayRR, out hit, 0.05f) + Input.GetKey(KeyCode.D));
+            Debug.Log("isClimbing: "+ isClimbing +"LL: " + Physics.Raycast(rayLL, out hit, 0.05f) + Input.GetKey(KeyCode.A) +  "____RR: " + Physics.Raycast(rayRR, out hit, 0.05f) + Input.GetKey(KeyCode.D));
 
-            if (Physics.Raycast(rayLL, out hit, 0.4f) && isClimbing && Input.GetKey(KeyCode.A)) {
+            if (Physics.Raycast(rayLL, out hit, distToSideWall) && isClimbing && Input.GetKey(KeyCode.A)) {
                 //Turn left when wall continue on left
                 transform.eulerAngles -= new Vector3(0, 90, 0);
                 //Debug.Log("Turn LEFT");
-            } else if (Physics.Raycast(rayRR, out hit, 0.4f) && isClimbing && Input.GetKey(KeyCode.D)) {
+            } else if (Physics.Raycast(rayRR, out hit, distToSideWall) && isClimbing && Input.GetKey(KeyCode.D)) {
                 //Turn right when wall continue on right
                 transform.eulerAngles += new Vector3(0, 90, 0);
                 //Debug.Log("Turn RIGHT");
             }
         } else {
-            if (!Physics.Raycast(ray, out hit, 0.6f) && Physics.Raycast(rayR, out hit, 0.6f) && isClimbing) {
+            if (!Physics.Raycast(ray, out hit, distToWall/*0.6f */) && Physics.Raycast(rayR, out hit, distToWall) && isClimbing) {
                 //Debug.Log("Climb Left");
-                //rotation pivot: transform.position -= ;
-                //transform.position = Pi
+                // * Time.deltaTime on the angle for smoothness but glitchy on transition to other wall / may fall               
                 transform.RotateAround(transform.position - transform.TransformVector(Vector3.forward * -0.5f), Vector3.up, 90f);
-            } else if (!Physics.Raycast(ray, out hit, 0.6f) && Physics.Raycast(rayL, out hit, 0.6f) && isClimbing) {
+            } else if (!Physics.Raycast(ray, out hit, distToWall) && Physics.Raycast(rayL, out hit, distToWall) && isClimbing) {
                 //Debug.Log("Climb Right");
                 transform.RotateAround(transform.position - transform.TransformVector(Vector3.forward * -0.5f), Vector3.up, -90f);
             } else if (isClimbing) {
                 //TODO: temp solution - up ladder ok, but down ladder need to stop the jump
-                transform.position += Vector3.up * moveSpeed * Time.deltaTime * 20;
+                transform.position += Vector3.up * moveSpeed * Time.deltaTime * 30;
                 //Debug.Log("TEST CHECK");
                 isClimbing = false;
             }
@@ -165,17 +180,21 @@ public class PlayerController : NetworkBehaviour {
         Vector3 rightMovement = climbRight * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
         Vector3 upMovement = up * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
-
+        
         if (heading != Vector3.zero) { //Prevent Vector3 = 0 error when jumping
             //transform.forward += heading * Time.deltaTime * rotationSpeed;
             transform.position += rightMovement;
             transform.position += upMovement;
+            //rb.velocity = new Vector3(heading.x * moveSpeed, heading.y * moveSpeed, heading.z * moveSpeed);
         }
 
         //Jump off ladder (temp solution)
         if (Input.GetKey(KeyCode.Space)) {
             //TODO: Wrong direction when jumping
-            transform.position += -Vector3.forward * moveSpeed * Time.deltaTime * 10;
+            isClimbing = false;
+            rb.AddForce(-transform.forward * 2f, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            //transform.position += -transform.forward * moveSpeed * Time.deltaTime;
         }
     }
 
